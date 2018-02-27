4374e177c3d5fe8f1abaa70e95363bda9b88dcfd
Richard Neumann <r.neumann@homeinfo.de>
2018-02-27 15:59:45 +0100

    Fixed add_routes parameters.

--- hievents/wsgi/__init__.py
+++ hievents/wsgi/__init__.py
@@ -6,5 +6,5 @@ from hievents.wsgi import customer, event, image, price, public, sub_event, tag
 
 APPLICATION = Application('hievents', debug=True, cors=True)
 APPLICATION.add_routes(
-    event.ROUTES + customer.ROUTES + image.ROUTES + price.ROUTES, public.ROUTES
-    + sub_event.ROUTES + tag.ROUTES)
+    event.ROUTES + customer.ROUTES + image.ROUTES + price.ROUTES
+    + public.ROUTES + sub_event.ROUTES + tag.ROUTES)
