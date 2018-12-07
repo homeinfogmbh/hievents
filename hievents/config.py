939e4bd780a6c30d597e3440a9d04bf9c40b93ca
Richard Neumann <r.neumann@homeinfo.de>
2018-12-07 12:25:06 +0100

    Refac.

--- hievents/config.py
+++ hievents/config.py
@@ -1,8 +1,9 @@
 """Configuration parser."""
 
-from configlib import INIParser
+from configlib import loadcfg
+
 
 __all__ = ['CONFIG']
 
 
-CONFIG = INIParser('/usr/local/etc/hievents.conf')
+CONFIG = loadcfg('hievents.conf')
