afe68a3078e5738af98cea0ae98f8b598dd3a232
Richard Neumann <r.neumann@homeinfo.de>
2019-11-06 10:19:34 +0100

    FIx.

--- hievents/wsgi/__init__.py
+++ hievents/wsgi/__init__.py
@@ -4,7 +4,7 @@ from wsgilib import Application
 
 from hievents.wsgi import customer, event, image, price, public, sub_event, tag
 
-APPLICATION = Application('hievents', debug=True, cors=True)
+APPLICATION = Application('hievents', debug=True)
 APPLICATION.add_routes(
     event.ROUTES + customer.ROUTES + image.ROUTES + price.ROUTES
     + public.ROUTES + sub_event.ROUTES + tag.ROUTES)
